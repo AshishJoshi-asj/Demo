dec48c9a7dbcf1ca4bc6a3c723eeac7d4873d15d
Travis <travis@travis-ci.org>
2015-06-16 19:03:17 +0530

    add helloTest class

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -1,12 +1,16 @@
 <?php
-
+/**
+ * @covers DefaultClass::ashishittechnosoft\Demo\Hello
+ */
 class HelloTest extends PHPUnit_Framework_TestCase
 {
     protected  $hello;
     public function setUp() {
         $this->hello = new ashishittechnosoft\Demo\Hello(); 
     }
-    
+    /*
+     * @cover ::world
+     */
     public function testWorld(){
         $this->assertSame('world',  $this->hello->world());
     }
